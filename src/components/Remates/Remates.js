9784b5099caa5263c1689f4786d550dea7b27f86
Manuel Alejandro Garcia <manuelalejandrogarcia@live.com.ar>
2023-06-07 15:16:59 -0300

    seccion remates actualizada

--- src/components/Remates/Remates.js
+++ src/components/Remates/Remates.js
@@ -31,45 +31,61 @@ const Card = () => {
     <div>
       <h1 ref={titleRef} className='title-remates' id='Remates' >Nuestros remates</h1>
       <div className="card" >
-        <div className="card-image">
-          <img src={rematesimg} alt="Imagen-card" />
-          <div className='content-card'>
-            <h4 className='texto-cantidad'>2500</h4>
-            <h4 className='texto-vacunos'>Vacunos</h4>
-            <h4 className='texto-footer'>Gordo, invernada y cria</h4>
+          <div className='card-column1'>
+            <img className='card-img' src={rematesimg} alt='fondocard'></img>
+            <div className='content-img'>
+                <h4 className='cantidad'>2500</h4>
+                <h4 className='vacunos'>Vacunos</h4>
+                <h4 className='tipo'>Gordo, invernada y cria</h4>
+            </div>
           </div>
-        </div>
-        <div className='remate-column1'>
-            <div>
-              <h4 className='remate-feria'>Remate feria Mensual</h4>
+          <div className='card-column2'>
+            <div className='row1'>
+              <div className='row1-remates'>
+                <h4 className='remate-feria'>Remate Feria</h4>
+                <h4 className='remate-mensual'>Mensual</h4> 
+              </div>
+              <div className='row1-fecha'> 
+                  <img src={calendario} alt="Calendario" />
+                  <h4>30 de Junio</h4>
+              </div>
             </div>
-            <div>
-              <h4 className='remate-fecha'>30 de julio</h4>
+            <div className='row2'>
+              <div>
+                  <h4 className='destacamos'>Destacamos</h4>
+                  <h4> 1500 TENEROS/ AS DE INVERADA <br></br> 60 VACAS NEGRAS Y COLORADAS<br></br>NUEVAS CON GARANTIA DE PREÑEZ</h4>
+              </div>
+              <div>
+                  <h4 className='importante'>IMPORTANTE!<br></br>las haciendas<br></br>deberan ingresar en<br></br>su totalidad el día<br></br>anterior sin<br></br>excepción</h4>
+              </div>
             </div>
-            
-        </div> 
+            <div className='row3'>
+              <div className='column1'>
+                <img src={ubi} alt="Calendario" />
+                <h4>Sociedad rural</h4>
+              </div>
+              <div className='column2'>
+                <img src={hora} alt="Calendario" />
+                <h4>13hs</h4>
+              </div>
+              <div className='column3'>
+                <img src={venta} alt="Calendario" />
+                <h4>venta contado y plazo</h4>
+              </div>
+            </div>
+            <div className='row4'>
+              <h4 className='boton-remate'>Ver remate</h4>
+            </div>
+          </div>
       </div>
 
-
-      <div className="card" >
-        <div className="card-image">
-          <img src={rematesimg} alt="Imagen de la tarjeta" />
-        </div>
-        <div className="card-content">
-          <h3>Título de la tarjeta</h3>
-          <p>Contenido de la tarjeta</p>
-        </div>
-        <div className="card-content">
-          <h3>Título de la tarjeta</h3>
-          <p>Contenido de la tarjeta</p>
-        </div>
-       
-        
+      <div className='row1'>
+              <div className='row1-remateferia'>
+                <h4>Remate Feria</h4>
+                <h4>Mensual</h4> 
+              </div>
       </div>
-      
-
-      
-
+   
       {/* Agrega más tarjetas aquí */}
     </div>
   );
